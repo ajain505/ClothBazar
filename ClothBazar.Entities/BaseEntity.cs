8a202389e0680def9d26a5f5dd93667c15304673
Aastha Kothari <aasthajain958@gmail.com>
2024-02-14 13:10:17 +0530

    adding database

---
+++ ClothBazar.Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Entities
+{
+    public class BaseEntity
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+
+    }
+}
